bd61dcc117d05212333ad46a8a20c1b1f4894566
vaskovicdejan <vaskovicdejan714@gmail.com>
2021-10-28 01:58:55 +0900

    Fix/drawer (#608)
    * Fix the drawer
    
    * Fix the drawer

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,65 +1,86 @@
-import React, { PropsWithChildren, useState } from 'react';
+import React from 'react';
 import {
   AppBar, Box, CssBaseline, Divider, Drawer, IconButton, List, ListItem, ListItemIcon, ListItemText, Toolbar, Typography,
 } from '@mui/material';
 import DashboardIcon from '@mui/icons-material/Dashboard';
 import MenuIcon from '@mui/icons-material/Menu';
+import {
+  useTheme,
+} from '@mui/material/styles';
 
 const drawerWidth = 240;
 
 interface LayoutProps {
-  /**
-   * Injected by the documentation to work in an iframe.
-   * You won't need it on your project.
-   */
-  container?: Element;
+  container?: Element
 }
 
-const Layout = (props: PropsWithChildren<LayoutProps>): JSX.Element => {
+const Layout: React.FC<LayoutProps> = (props) => {
   const { container } = props;
-  const [drawerOpen, setDrawerOpen] = useState(false);
+  const theme = useTheme();
+  const [mobileOpen, setMobileOpen] = React.useState(false);
 
   function handleDrawerToggle() {
-    setDrawerOpen(!drawerOpen);
+    setMobileOpen(!mobileOpen);
   }
 
+  const drawer = (
+    <Box sx={{ width: drawerWidth, backgroundColor: theme.palette.primary.main, color: theme.palette.primary.contrastText }}>
+      <Box sx={theme.mixins.toolbar} />
+      <Divider />
+      <List>
+        {['Info'].map((text) => (
+          <ListItem button key={text} sx={{
+            width: drawerWidth,
+            backgroundColor: theme.palette.primary.main,
+            '&:hover': {
+              backgroundColor: theme.palette.primary.light,
+            },
+          }}>
+            <ListItemIcon>
+              <DashboardIcon sx={{ color: theme.palette.primary.contrastText }}/>
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
   return (
-    <Box sx={{ display: 'flex', color: '#fff' }}>
+    <Box sx={{ display: 'flex', color: theme.palette.primary.contrastText }}>
       <CssBaseline />
-      <AppBar
-        position="fixed"
-        sx={{ marginLeft: drawerWidth, width: `calc(100% - ${drawerWidth}px)` }}
-      >
+      <AppBar position="fixed" sx={{ width: '100%' }}>
         <Toolbar>
           <IconButton
             color="inherit"
             aria-label="open drawer"
             edge="start"
             onClick={handleDrawerToggle}
-            sx={{ marginRight: '2em', display: 'none' }}
           >
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" noWrap></Typography>
         </Toolbar>
       </AppBar>
-      <Drawer container={container} variant="permanent" open>
-        <Divider />
-        <List sx={{ width: drawerWidth }}>
-          {['Info'].map((text) => (
-            <ListItem button key={text} sx={{ color: '#2d2d2d' }}>
-              <ListItemIcon sx={{ color: '#2d2d2d' }}>
-                <DashboardIcon />
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
-      </Drawer>
-      <Box style={{ flexGrow: 1 }}>
-        <Box />
-        {props.children}
+      <Box component='nav' sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } } } aria-label="folders">
+        <Drawer
+          container={container}
+          variant="temporary"
+          anchor='left'
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          sx={{
+            '& .MuiDrawer-paper': {  backgroundColor: theme.palette.primary.main, boxSizing: 'border-box', width: drawerWidth },
+          }}
+          ModalProps={{ keepMounted: true }}
+        >
+          {drawer}
+        </Drawer>
       </Box>
+      <main style={{ flexGrow: 1 }}>
+        <Box sx={theme.mixins.toolbar} />
+        {props.children}
+      </main>
     </Box>
   );
 };
