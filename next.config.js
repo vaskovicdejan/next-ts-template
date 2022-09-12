09e9775d6646d6db8ce441e4839bf761d5123ca8
vaskovicdejan <vaskovicdejan714@gmail.com>
2022-09-12 21:52:12 +0900

    Bumps node modules

--- next.config.js
+++ next.config.js
@@ -1,15 +1,12 @@
-/** @type {import('next').NextConfig} */
 const withBundleAnalyzer = require('@next/bundle-analyzer');
 
+/** @type {import('next').NextConfig} */
 const config = withBundleAnalyzer({
   enabled: process.env.ANALYZE === 'true',
 })(
   {
     reactStrictMode: true,
     swcMinify: true,
-    experimental: {
-      esmExternals: true,
-    }
   }
 );
 
