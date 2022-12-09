bbba047456bbf0c1b19fda61ec99bec8a162e5c8
vaskovicdejan <vaskovicdejan714@gmail.com>
2022-12-09 21:47:19 +0900

    Bumps node modules

---
+++ update.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+CUR=$(pwd)
+
+CURRENT=$(cd $(dirname $0);pwd)
+echo "${CURRENT}"
+
+cd "${CURRENT}"
+git pull --prune
+result=$?
+if [ $result -ne 0 ]; then
+  cd "${CUR}"
+  exit $result
+fi
+echo ""
+pwd
+rm -rf node_modules yarn.lock .yarn/cache && touch yarn.lock && yarn install && yarn up -R && yarn lint --fix && yarn all
+result=$?
+if [ $result -ne 0 ]; then
+  cd "${CUR}"
+  exit $result
+fi
+git commit -am "Bumps node modules" && git push
+result=$?
+if [ $result -ne 0 ]; then
+  cd "${CUR}"
+  exit $result
+fi
+
+cd "${CUR}"
