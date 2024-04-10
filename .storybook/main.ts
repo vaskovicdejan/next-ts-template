fbf41d3e31f41c773292cdacd4e5f0ef759b512b
vaskovicdejan <vaskovicdejan714@gmail.com>
2024-04-10 20:03:35 +0900

    Fix the storybokk wornings (#2495)

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -7,9 +7,31 @@ const config: StorybookConfig = {
     '@storybook/addon-essentials',
     '@storybook/addon-onboarding',
     '@storybook/addon-interactions',
+    "@storybook/addon-themes",
     {
-      name: '@storybook/addon-styling',
-      options: {},
+      name: "@storybook/addon-styling-webpack",
+       options: {
+         rules: [
+           // Replaces existing CSS rules to support PostCSS
+           {
+             test: /\.css$/,
+             use: [
+               "style-loader",
+               {
+                 loader: "css-loader",
+                 options: { importLoaders: 1 },
+               },
+               {
+                 // Gets options from `postcss.config.js` in +your project root
+                 loader: "postcss-loader",
+                 options: {
+                   implementation: require.resolve("postcss"),
+                 },
+               },
+             ],
+           },
+         ],
+       },
     },
   ],
   framework: {
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,8 +1,6 @@
 import type { Preview } from '@storybook/react';
 import { initialize, mswLoader } from 'msw-storybook-addon';
 
-import { withThemeByClassName } from '@storybook/addon-styling';
-
 import '../src/app/globals.css';
 
 // Initialize MSW
@@ -20,15 +18,6 @@ const preview: Preview = {
   },
 
   decorators: [
-    // Adds theme switching support.
-    // NOTE: requires setting "darkMode" to "class" in your tailwind config
-    withThemeByClassName({
-      themes: {
-        light: 'light',
-        dark: 'dark',
-      },
-      defaultTheme: 'light',
-    }),
   ],
   // Provide the MSW addon loader globally
   loaders: [mswLoader],
